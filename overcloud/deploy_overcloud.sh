4e9971b36d3fd0d2cf1674968e31953bc9f097fd
Ruslanas Gžibovskis <lpic.lt@lpic.lt>
2020-05-12 11:17:12 +0200

    update cloud deployment with disable telemetry and

--- overcloud/deploy_overcloud.sh
+++ overcloud/deploy_overcloud.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 
 source /home/stack/stackrc
-cd /home/stack
-
-openstack overcloud deploy \
-  --templates \
-  -r /home/stack/overcloud/aio_roles_data.yaml \
-  -n /home/stack/overcloud/network_data_empty.yaml \
-  -e /usr/share/openstack-tripleo-heat-templates/environments/enable-swap.yaml \
-  -e /home/stack/overcloud/nodes.yaml
+# cd /home/stack
+#
+#openstack overcloud deploy \
+#  --templates \
+#  -r /home/stack/overcloud/aio_roles_data.yaml \
+#  -n /home/stack/overcloud/network_data_empty.yaml \
+#  -e /usr/share/openstack-tripleo-heat-templates/environments/enable-swap.yaml \
+#  -e /home/stack/overcloud/nodes.yaml
   
+_THT="/usr/share/openstack-tripleo-heat-templates"
+_LTHT="$(pwd)"
+
+#time openstack overcloud deploy \
+#time openstack --debug overcloud deploy \
+time openstack --verbose overcloud deploy \
+    --force-postconfig \
+    --templates \
+    --stack boobs \
+    -r ${_LTHT}/roles_data.yaml \
+    -n ${_LTHT}/network_data.yaml \
+    -e ${_LTHT}/node-info.yaml \
+    -e ${_LTHT}/private.yaml \
+    -e ${_LTHT}/containers-prepare-parameter.yaml \
+    -e ${_LTHT}/overcloud_images.yaml \
+    -e ${_THT}/environments/disable-telemetry.yaml \
+    -e ${_THT}/environments/host-config-and-reboot.yaml \
+    --ntp-server pool.debian.org
+
+
